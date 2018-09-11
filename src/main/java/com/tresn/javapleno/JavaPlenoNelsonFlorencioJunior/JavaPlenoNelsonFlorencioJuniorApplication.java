aac2deb9515db30ec2b2968329b5a64690515e08
Nelson Florêncio Júnior <3nelsonjunior@gmail.com>
2018-09-10 22:27:46 -0300

    Primeiro Commit

---
+++ src/main/java/com/tresn/javapleno/JavaPlenoNelsonFlorencioJunior/JavaPlenoNelsonFlorencioJuniorApplication.java
@@ -0,0 +1,12 @@
+package com.tresn.javapleno.JavaPlenoNelsonFlorencioJunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPlenoNelsonFlorencioJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPlenoNelsonFlorencioJuniorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tresn/javapleno/JavaPlenoNelsonFlorencioJunior/JavaPlenoNelsonFlorencioJuniorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tresn.javapleno.JavaPlenoNelsonFlorencioJunior;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaPlenoNelsonFlorencioJuniorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
